220dbe53d844416631edc7c18ef4dd1328abde07
殷啟聰 | Kai-Chung Yan <seamlikok@gmail.com>
2018-06-05 22:07:44 +0800

    Implement ToBytes for Response

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,13 @@
+extern crate bytes;
+
+use bytes::BufMut;
 use std::net::IpAddr;
 
 pub const VERSION: u8 = 2;
+pub const PORT_CLIENT: u16 = 5350;
+pub const PORT_SERVER: u16 = 5351;
 
-trait ToBytes {
+pub trait ToBytes {
     fn length(&self) -> usize;
     fn to_bytes(&self) -> Vec<u8>;
 }
@@ -40,4 +45,25 @@ pub struct Response {
     pub lifetime: u32,
     pub result_code: u8,
     pub version: u8
+}
+
+impl ToBytes for Response {
+
+    fn length(&self) -> usize {
+        32 * 6 // TODO: Support Opcode data and options
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(self.length());
+        bytes.put_u8(self.version);
+        bytes.put_u8(0b10000000); // TODO: Support real Opcode
+        bytes.put_u8(0);
+        bytes.put_u8(self.result_code);
+        bytes.put_u32_be(self.lifetime);
+        bytes.put_u32_be(self.epoch);
+        bytes.put_slice(&[0; 96 / 8]);
+        // TODO: Support Opcode data and options
+
+        bytes
+    }
 }
